903edbd345fa1d1491bc2a3acaa44d43ee8c70ce
Nick Wilburn <senior.crepe@gmail.com>
2019-04-10 14:54:30 -0500

    WIP

--- template/python3-flask/index.py
+++ template/python3-flask/index.py
@@ -1,12 +1,20 @@
 # Copyright (c) Alex Ellis 2017. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-from flask import Flask, request
+from flask import Flask, request, Response
 from function import handler
 #from gevent.wsgi import WSGIServer
 from gevent.pywsgi import WSGIServer
+from gevent.queue import Queue
+from gevent.pool import Pool
+from gevent.pywsgi import StreamServer
+import json
+import random
+import gevent
 
 app = Flask(__name__)
+event_queue = Queue()
+
 
 @app.before_request
 def fix_transfer_encoding():
@@ -20,14 +28,31 @@ def fix_transfer_encoding():
     if transfer_encoding == u"chunked":
         request.environ["wsgi.input_terminated"] = True
 
+def get_events():
+    event = event_queue.get()
+    return event
+
+@app.route("/stream", methods=["GET"])
+def stream():
+    #if event_queue.empty():
+    #    return "Queue is empty"
+    def event_stream():
+        #try:
+        while True:
+            die_rolls = {u"1D6": random.randint(1, 6),
+                        u"1D20": random.randint(1, 20)}
+            yield "data: " + json.dumps(die_rolls) + "nn"
+
+        with app.app_context():
+            gevent.sleep(0.2)
+        #except GeneratorError:
+        #    print("nope")
+    return Response(event_stream(), mimetype="text/event-stream")
+
 @app.route("/", defaults={"path": ""}, methods=["POST", "GET"])
 @app.route("/<path:path>", methods=["POST", "GET"])
 def main_route(path):
-    ret = handler.handle(request)
-    return ret
-
-if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=5000, debug=False)
-
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    zip_file = request.files.getlist('files')[0]
+    job = gevent.spawn(handler.handle, zip_file, event_queue)
+    job.get(timeout=300)
+    return 'OK'
